2071ca14c0a5c6bd2b3b8836d0d61747ecf265f7
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-05-03 22:57:49 -0400

    commit tree f7478694daa0926c361c00fe77e1f1519432d250

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      # pirate = {:name => params[:pirate][:name], :weight => params[:pirate][:weight], :height => params[:pirate][:weight]}
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each {|i| Ship.new(i)}
+      @ships = Ship.all
+
+       erb :'pirates/show'
+    end
+
+    get '/pirates' do
+
+      erb :'pirates/show'
+    end
+
 
     # code other routes/actions here
 
